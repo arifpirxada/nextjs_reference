55c3313af9f908b02de66d67c2b1160329ee2873
arifpirxada <pirxadaarif@gmail.com>
2025-08-04 15:03:17 +0530

    Add - Blog, About and Contact Page in 04_pages_router

---
+++ 04_pages_router/pages/about/index.tsx
@@ -0,0 +1,18 @@
+const AboutPage = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-center text-2xl font-bold">About Us</h1>
+            <p className="mt-4 px-20 text-center">
+                Welcome to our blog!
+
+                We’re a passionate team of writers, thinkers, and explorers committed to sharing ideas, inspiration, and honest stories. Our mission is to create a space where curiosity thrives and knowledge is shared—one blog post at a time.
+
+                Whether you’re here for helpful tips, fresh perspectives, or just a bit of fun, we hope you’ll feel right at home. Our content covers everything from lifestyle and technology to creativity, wellness, and more. We believe in the power of storytelling and learning together as a community.
+
+                Thanks for stopping by! Stay curious, stay inspired, and happy reading.
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ 04_pages_router/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-center text-2xl font-bold">Our Blogs</h1>
+        </div>
+    )
+}
+
+export default BlogPage
